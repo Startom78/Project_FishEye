2520251114b5ebeaef372e29eb03a621b34cfb4d
Startom78 <tom78630@gmail.com>
2024-09-30 14:10:54 +0200

    add stylization of informations on main page

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -11,20 +11,16 @@ export const photographerTemplate = (data) => {
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         const h3 = document.createElement( 'h3' );
-        h3.textContent = city + (", ");
-        h3.classList.add("shadow")
+        h3.textContent = city + (", ") + country;
         const h4 = document.createElement( 'h4' );
-        h4.textContent = country;
+        h4.textContent = tagline;
         const h5 = document.createElement( 'h5' );
-        h5.textContent = tagline;
-        const h6 = document.createElement( 'h6' );
-        h6.textContent = price + ("€/jour");
+        h5.textContent = price + ("€/jour");
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(h3);
         article.appendChild(h4);
         article.appendChild(h5);
-        article.appendChild(h6);
         return (article);
     }
     return { name, picture, city, country, tagline, price, getUserCardDOM }
