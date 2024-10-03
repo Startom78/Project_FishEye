e0a4bea503a2b19fdf1f8d30025398c3b0466bfe
Startom78 <tom78630@gmail.com>
2024-10-03 11:50:52 +0200

    add link on img and name, add inputs in modal

--- scripts/api/api.js
+++ scripts/api/api.js
@@ -11,6 +11,8 @@ const API = {
             return {
                 photographer: data.photographers.find(p => p.id === photographerId),
                 media: data.media.filter(m => m.photographerId === photographerId),
+                city: data.photographers.find(c => c.id === photographerId).city,
+                country: data.photographers.find(co => co.id === photographerId).country,
                 tagline: data.photographers.find(t => t.id === photographerId).tagline,
                 price: data.photographers.find(pr => pr.id === photographerId).price
             }
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -5,19 +5,26 @@ export const photographerTemplate = (data) => {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const photographerUrl = "photographer.html";
+        const link = document.createElement('a');
+        link.setAttribute('href', photographerUrl);
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         img.classList.add("shadow")
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        //img.setAttribute("alt", `photo de profil de ${data.name}`)
+        link.appendChild(img);
+        link.appendChild(h2);
         const h3 = document.createElement( 'h3' );
         h3.textContent = city + (", ") + country;
         const h4 = document.createElement( 'h4' );
         h4.textContent = tagline;
         const h5 = document.createElement( 'h5' );
         h5.textContent = price + ("€/jour");
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(link);
+        //article.appendChild(img);
+        //article.appendChild(h2);
         article.appendChild(h3);
         article.appendChild(h4);
         article.appendChild(h5);
