a7d34537d44d6ad6720c172cb59470bfb34963cf
Startom78 <tom78630@gmail.com>
2024-11-22 15:28:59 +0100

    add dropDown files

---
+++ scripts/components/dropDownMenu/dropDown.js
@@ -0,0 +1,7 @@
+export const dropDown = (options, onChange) => {
+  //step 1 : créer contenaire pos relative
+  //step 2 : créer contenaire pour les options
+  //step 3 : parcourir options (array) et pour chaque valeur, créer une option (absolute)
+  //step 4 : l'event listener sur le chevron (pour débloquer contenaire step 2)
+  //step 5 : pour chaque option un event.listener sur le onClick (faire passer la valeur cliquée tout en haut du tableau d'option et doit appeler fonction onChange avec la valeur cliquée)
+};
